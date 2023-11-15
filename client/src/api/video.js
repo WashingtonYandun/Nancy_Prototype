f80cc93def117cf8d6030e70a245e2c2b0768541
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2023-11-15 12:09:16 -0500

    feat: Client => Videos funtionality implemented

--- client/src/api/video.js
+++ client/src/api/video.js
@@ -1,19 +1,21 @@
 import axios from "./axios";
 
-export const getAllVideosRequest = async () => axios.get("/videos/all");
+export const getAllVideosRequest = async () => axios.get("/admin/videos/all");
 
 export const getVideoByUserRequest = async (id) =>
-    axios.get(`/videos/user/${id}`);
+    axios.get(`/admin/videos/user/${id}`);
 
 export const getVideoByCategoryRequest = async (category) => {
-    axios.get(`/videos/category/${category}`);
+    axios.get(`/admin/videos/category/${category}`);
 };
 
-export const createVideoRequest = async (video) => axios.post("/videos", video);
+export const createVideoRequest = async (video) =>
+    axios.post("/admin/videos", video);
 
 export const updateVideoRequest = async (id, video) =>
-    axios.put(`/videos/${id}`, video);
+    axios.put(`/admin/videos/${id}`, video);
 
-export const deleteVideoRequest = async (id) => axios.delete(`/videos/${id}`);
+export const deleteVideoRequest = async (id) =>
+    axios.delete(`/admin/videos/${id}`);
 
-export const getVideoRequest = async (id) => axios.get(`/videos/${id}`);
+export const getVideoRequest = async (id) => axios.get(`/admin/videos/${id}`);
