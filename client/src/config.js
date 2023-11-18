a6137085b83706627f205d04ccc5611fa1492a35
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2023-11-18 17:17:31 -0500

    chore: Update back url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
-    import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+    import.meta.env.VITE_API_URL || "https://nancy-back.onrender.com";
 
 export const WY_APIS = "https://nancy-classifier-wy.vercel.app";
